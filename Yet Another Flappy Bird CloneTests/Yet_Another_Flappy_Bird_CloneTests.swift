3bae2054514223e6b7a0797b61a167b5b2c7c004
Bartosz Osowski <bartosz.osowski@errigal.com>
2018-09-12 16:46:11 +0100

    Finished game.

---
+++ Yet Another Flappy Bird CloneTests/Yet_Another_Flappy_Bird_CloneTests.swift
@@ -0,0 +1,36 @@
+//
+//  Yet_Another_Flappy_Bird_CloneTests.swift
+//  Yet Another Flappy Bird CloneTests
+//
+//  Created by Bartosz Osowski on 12/09/2018.
+//  Copyright © 2018 Bartosz Osowski. All rights reserved.
+//
+
+import XCTest
+@testable import Yet_Another_Flappy_Bird_Clone
+
+class Yet_Another_Flappy_Bird_CloneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
